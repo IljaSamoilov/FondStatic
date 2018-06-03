ce452c92a68fe485fad8b2feef421af9d71827be
Ilja Samoilov <ilsamoilov@gmail.com>
2018-06-03 20:26:54 +0300

    spending analyzer update

---
+++ SpendingAnalyzer/src/main/java/ee/ilja/samoilov/constants/Constants.java
@@ -0,0 +1,9 @@
+package ee.ilja.samoilov.constants;
+
+public class Constants {
+
+    public static final String DEBIT = "D";
+    public static final String CREDIT = "K";
+    public static final String TRANSACTION = "20";
+
+}
--- SpendingAnalyzer/src/main/java/ee/ilja/samoilov/service/DataProcessingHandler.java
+++ SpendingAnalyzer/src/main/java/ee/ilja/samoilov/service/DataProcessingHandler.java
@@ -1,7 +1,26 @@
 package ee.ilja.samoilov.service;
 
+import ee.ilja.samoilov.constants.Constants;
+import ee.ilja.samoilov.containers.Transaction;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 /**
  * Created by Ilja on 13.01.2018.
  */
+@Service
 public class DataProcessingHandler {
+
+    public void parseTransaction(String[] line, ArrayList<Transaction> transactions) {
+        if (line[2].equals(Constants.TRANSACTION)) {
+            boolean isDebit = line[7].equals(Constants.DEBIT);
+            transactions.add(new Transaction(new BigDecimal(line[5]), line[3], isDebit));
+        }
+    }
+
+    public void sendTransactions(ArrayList<Transaction> transactions) {
+        //TODO send transaction list to database
+    }
 }
